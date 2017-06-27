f5fc5536ead65ba514d48a170b6e0c3b69c6da61
Taryn Hill <taryn@phrohdoh.com>
2017-06-27 18:43:28 -0500

    Re-export the byteorder crate
    Bump to 0.1.4

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-extern crate byteorder;
+pub extern crate byteorder;
 extern crate flate2;
 
 use byteorder::LittleEndian;
